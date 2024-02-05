a7ac518f49c7ac8b078eda09e0ae14fab6266ffb
sHKamil <shootekb@gmail.com>
2024-02-05 02:55:26 +0100

    endpoint with dummy data for frontend

--- symfony/src/Controller/ApiController.php
+++ symfony/src/Controller/ApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Vehicles;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +17,29 @@ class ApiController extends AbstractController
             'message' => 'Welcome from Symfony!',
             'path' => 'src/Controller/ApiController.php',
         ]);
+    }
+
+    #[Route('/vehicles', name: 'dummy_vehicle', methods: ['GET'])]
+    public function dummyVehicle(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $repository = $entityManager->getRepository(Vehicles::class);
+        $allMessages = $repository->findAll();
+        $result = [];
+        foreach ($allMessages as $message) {
+            $result[] = [
+                'id' => $message->getId(),
+                'name' => $message->getName(),
+                'type' => $message->getType(),
+                'size'=> $message->getSize(),
+                'color' => $message->getColor(),
+                'image' => $message->getImage(),
+                'quantity' => $message->getQuantity(),
+                'available'=> $message->getAvailable(),
+                'wheel_size' => $message->getWheelSize(),
+                'price' => $message->getPrice()
+            ];
+        }
+
+        return $this->json($result);
     }
 }
